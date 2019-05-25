8b140742fa78134ca3531e203bb9b0feb04a68d3
John DeWyze <1312168+dewyze@users.noreply.github.com>
2019-05-25 11:55:29 -0500

    WIP - dev server working

--- renderer.js
+++ renderer.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { hello } from "./hot_test";
 
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello World!</h1>;
-  }
+// class Hello extends React.Component {
+//   render() {
+//     return <h1>Hello World!</h1>;
+//   }
+// }
+
+// ReactDOM.render(<Hello />, document.getElementById("app"));
+const app = document.getElementById("app");
+app.innerHTML = hello();
+
+if (module.hot) {
+  console.log("MODULE IS HOT");
+  module.hot.accept("./hot_test.js", () => {
+    console.log("Accepting the updated printMe module!");
+    console.log(hello());
+    app.innerHTML = hello();
+  });
 }
-
-ReactDOM.render(<Hello />, document.getElementById("app"));
