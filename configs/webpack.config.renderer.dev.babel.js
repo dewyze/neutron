5fe4fdbc3620d99980d9f1605c65d7f53a20efe4
John DeWyze <1312168+dewyze@users.noreply.github.com>
2019-05-25 09:41:36 -0500

    Allow DLL to be built before it's built
    Previously, if you tried to build the dll before it was built, there was
    an error in the non-dll file because it couldn't find it. This is
    because the dll file uses the modules object from the non-dll object, so
    that file needs to be compiled for it to work.
    
    Instead, we check to see if the requiring filename is the dll or non-dll
    file. If it is the non-dll file we don't include the DLLReferencePlugin,
    otherwise we do and then everything builds fine.

--- configs/webpack.config.renderer.dev.babel.js
+++ configs/webpack.config.renderer.dev.babel.js
@@ -1,4 +1,7 @@
 import baseConfig from "./webpack.config.base";
+import chalk from "chalk";
+import { execSync } from "child_process";
+import fs from "fs";
 import merge from "webpack-merge";
 import path from "path";
 import webpack from "webpack";
@@ -6,6 +9,21 @@ import webpack from "webpack";
 const dist = path.join(__dirname, "..", "dist");
 const dll = path.join(__dirname, "..", "dll");
 const manifest = path.resolve(dll, "renderer.json");
+const requiredByDLLConfig = module.parent.filename.includes(
+  "webpack.config.renderer.dev.dll"
+);
+
+/**
+ * Warn if the DLL is not built
+ */
+if (!requiredByDLLConfig && !(fs.existsSync(dll) && fs.existsSync(manifest))) {
+  console.log(
+    chalk.black.bgYellow.bold(
+      'The DLL files are missing. Sit back while we build them for you with "npm run build-dll"'
+    )
+  );
+  execSync("npm run build-dll");
+}
 
 export default merge.smart(baseConfig, {
   devtool: "inline-source-map",
@@ -31,11 +49,13 @@ export default merge.smart(baseConfig, {
   },
 
   plugins: [
-    new webpack.DllReferencePlugin({
-      context: path.join(__dirname, "..", "dll"),
-      manifest: require(manifest),
-      sourceType: "var"
-    })
+    requiredByDLLConfig
+      ? null
+      : new webpack.DllReferencePlugin({
+          context: path.join(__dirname, "..", "dll"),
+          manifest: require(manifest),
+          sourceType: "var"
+        })
   ],
 
   node: {
